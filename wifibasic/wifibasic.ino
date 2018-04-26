52d4b8fa6f17cd4ee7ade047fe63caa6c9a67daf
Stephen James <sjames@examsoft.com>
2018-04-26 05:23:37 -0500

    Receive response

--- wifibasic/wifibasic.ino
+++ wifibasic/wifibasic.ino
@@ -7,83 +7,89 @@
 #define PINGCMD 2
 #define ACK 0x7f
 
-#define nop asm volatile ("nop\n\t")
+#define nop asm volatile("nop\n\t")
 
-const char* ssid = "ATTVMb9amS";
-const char* password = "xmcpmjhvr7u7";
- 
+const char *ssid = "ATTVMb9amS";
+const char *password = "xmcpmjhvr7u7";
+
 int ledPin = LED_BUILTIN;
 int resetPin = D1;
 WiFiServer server(9000);
 WiFiClient client;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   delay(10);
- 
+
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, HIGH);
 
   SPI.begin();
- 
+
   // Connect to WiFi network
   Serial.println();
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
- 
+
   WiFi.begin(ssid, password);
- 
-  while (WiFi.status() != WL_CONNECTED) {
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
   Serial.println("WiFi connected");
- 
+
   // Start the server
   server.begin();
   Serial.println("Server started");
- 
+
   // Print the IP address
   Serial.print("Use this IP to connect: ");
   Serial.println(WiFi.localIP());
 
   resetSlave();
-  
+
   delay(2000);
   String testString = "SPI Interface Initialized\n";
   SPI.transfer((char)PINGCMD);
   debugMsgInt("sending command: ", PINGCMD);
   SPI.transfer((char)testString.length());
   debugMsgInt("sending length: ", testString.length());
-  
-  for (int i = 0; i < testString.length(); i++) {
+
+  for (int i = 0; i < testString.length(); i++)
+  {
     SPI.transfer(testString[i]);
   }
-  delay(10); 
+  delay(10);
   SPI.transfer(0xFF);
   int cmd = SPI.transfer(0xFF);
   int len = SPI.transfer(0xFF);
 
   debugMsgInt("Rcv Command: ", cmd);
   debugMsgInt("Length: ", len);
-  
 }
- 
-void loop() {
+
+void loop()
+{
   // Check if a client has connected
-  if (!client.connected()) {
+  if (!client.connected())
+  {
     client = server.available();
-    if (!client) {
+    if (!client)
+    {
       delay(10);
       return;
     }
     Serial.println("new client");
   }
- 
+
   // Wait until the client sends some data
-  if (!client.available()){
+  if (!client.available())
+  {
     delay(1);
     return;
   }
@@ -95,22 +101,27 @@ void loop() {
   debugMsgInt("Length: ", len);
 
   String request;
-  if (len > 0) {
+  if (len > 0)
+  {
     request = readFor(&client, len);
   }
-  
-  if (cmd == RESETCMD) {
+
+  if (cmd == RESETCMD)
+  {
     resetSlave();
   }
-  else {
+  else
+  {
     SPI.transfer((char)cmd);
     // TODO: this needs to be send var int
-    SPI.transfer((char) request.length());
-    for (int i = 0; i < request.length(); i++) {
+    SPI.transfer((char)request.length());
+    for (int i = 0; i < request.length(); i++)
+    {
       SPI.transfer(request[i]);
     }
   }
-  
+
+  // Wait a short bit and receive response
   delay(10);
   SPI.transfer(0xFF);
   cmd = SPI.transfer(0xFF);
@@ -118,37 +129,60 @@ void loop() {
 
   debugMsgInt("Rcv Command: ", cmd);
   debugMsgInt("Length: ", len);
-  delay(1); 
+  delay(1);
+  String response;
+  for (int i = 0; i < len; i++)
+  {
+    char c = SPI.transfer(0xFF);
+    response += c;
+  }
+  debugMsgStr("Rcv Command data: ", response);
+
+  // TODO - send to TCP client
 }
 
-int readVarint(WiFiClient* client) {
+int readVarint(WiFiClient *client)
+{
   unsigned int value = 0;
   int len = 0;
-  while (true) {
-    while(!client->available()) {
+  while (true)
+  {
+    while (!client->available())
+    {
       delay(1);
     }
     unsigned char c = client->read();
     value = ((c & 0x7f) << (len * 7)) + value;
-    if (c < 128) break;
+    if (c < 128)
+      break;
   }
-  if (DEBUG) { Serial.print("Varint: "); Serial.println(value); }
+  if (DEBUG)
+  {
+    Serial.print("Varint: ");
+    Serial.println(value);
+  }
   return value;
 }
 
-void sendVarint(WiFiClient* client, unsigned int value) {
-  do {
-    unsigned char c = value &0x7f;
+void sendVarint(WiFiClient *client, unsigned int value)
+{
+  do
+  {
+    unsigned char c = value & 0x7f;
     value = value >> 7;
-    if (value > 0) c = c | 0x80;
+    if (value > 0)
+      c = c | 0x80;
     client->write(c);
   } while (value > 0);
 }
 
-String readFor(WiFiClient* client, unsigned int len) {
+String readFor(WiFiClient *client, unsigned int len)
+{
   String buff = "";
-  while (len-- > 0) {
-    while(!client->available()) {
+  while (len-- > 0)
+  {
+    while (!client->available())
+    {
       delay(1);
     }
     char c = client->read();
@@ -157,24 +191,36 @@ String readFor(WiFiClient* client, unsigned int len) {
   return buff;
 }
 
-void writeFor(WiFiClient* client, String buffer) {
+void writeFor(WiFiClient *client, String buffer)
+{
   int ndx = 0;
-  while (ndx < buffer.length()) {
+  while (ndx < buffer.length())
+  {
     client->write(buffer[ndx++]);
   }
 }
 
-void resetSlave() {
+void resetSlave()
+{
   pinMode(resetPin, OUTPUT);
   digitalWrite(resetPin, LOW);
   delay(500);
   digitalWrite(resetPin, HIGH);
 }
-  
-void debugMsgInt(const char* msg, int value) {
-  if (DEBUG) {
+
+void debugMsgInt(const char *msg, int value)
+{
+  if (DEBUG)
+  {
     Serial.print(msg);
     Serial.println(value);
   }
 }
 
+void debugMsgStr(const char *msg, String data)
+{
+  if (DEBUG)
+  {
+    Serial.print(msg + data);
+  }
+}
